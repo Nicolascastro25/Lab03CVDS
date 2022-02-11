7468f0eaf4a9047ecda8f9f8c7a2bf24b0f1699d
Nicolascastro25 <jaime.nicolas.castro@gmail.com>
2022-02-10 22:48:03 -0500

    Parrafo finalizar el ejercicio realizado

--- ClasesEquivalencia/src/main/java/edu/eci/cvds/tdd/registry/Registry.java
+++ ClasesEquivalencia/src/main/java/edu/eci/cvds/tdd/registry/Registry.java
@@ -2,8 +2,27 @@ package edu.eci.cvds.tdd.registry;
 
 public class Registry {
     public RegisterResult registerVoter(Person p) {
-
-        // TODO Validate person and return real result.
+		Person prueba = new Person("Nicolas", 1000225263, 22, Gender.MALE , true);
+		//clase dead
+		if(p.isAlive()==false){
+			return RegisterResult.DEAD;
+		}
+		//clase sin edad suficiente
+		else if(p.getAge >= 0 && p.getAge <= 17 && p.isAlive()==true){
+			return RegisterResult.UNDERAGE;
+		}
+		//clase edad invalida
+		else if(p.getAge <= 0 || p.getAge >= 120 && p.isAlive()==true){
+			return RegisterResult.INVALID_AGE;
+		}
+		//clase edad valida
+		else if(p.getAge >= 18 && p.getAge <= 120 && p.isAlive()==true){
+			return RegisterResult.VALID;
+		}
+		//clase edad duplicada
+		else if (p.getId()==prueba.getId() && p.isAlive()==true){
+			return RegisterResult.DUPLICATED;
+		}
         return RegisterResult.VALID;
     }
 }
--- ClasesEquivalencia/src/test/java/edu/eci/cvds/tdd/registry/RegistryTest.java
+++ ClasesEquivalencia/src/test/java/edu/eci/cvds/tdd/registry/RegistryTest.java
@@ -9,22 +9,63 @@ public class RegistryTest {
 
     @Test
     public void validateRegistryResult() {
-
         Person person = new Person();
-
         RegisterResult result = registry.registerVoter(person);
-
         Assert.assertEquals(RegisterResult.VALID, result);
     }
-
-    // TODO Complete with more test cases
-
+	/*
+	* Prueba donde se crea una persona muerta y se intenta registrar
+	*/	
     @Test
-    public void validateRegistryResultDEAD(){
-        Person person = new Person("valentina",10101010,20,Gender.FEMALE,false);
+    public void validateRegistryResultDead(){
+        Person person = new Person("valentina",1000254541,20,Gender.FEMALE,false);
         RegisterResult result = registry.registerVoter(person);
         Assert.assertEquals(RegisterResult.DEAD,result);
     }
+	/*
+	* Prueba donde se crea una persona menor de edad y se intenta registrar
+	*/
     @Test
-    public void ValidateRegistryResultUNDERAGE(){}
+    public void validateRegistryResultUnderage(){
+		Person person = new Person("Nicolas", 10002256325, 17, Gender.MALE , true);
+        RegisterResult result = registry.registerVoter(person);
+        Assert.assertEquals(RegisterResult.UNDERAGE, result);
+    }
+	/*
+	* Prueba donde se crea una persona con edad invalida y se intenta registrar
+	*/
+	@Test
+	public void validateRegistryResultInvalidAge(){
+		Person person = new Person("Santiago", 22563250, 150, Gender.MALE , true);
+		RegisterResult result = registry.registerVoter(person);
+		Assert.assertEquals(RegisterResult.INVALID_AGE, result);
+	}
+	/*
+	* Prueba donde se crea una persona valida y se intenta registrar
+	*/
+	@Test
+	public void validateRegistryResultValid(){
+		Person person = new Person("Laura", 56449400, 35, Gender.MALE , true);
+		RegisterResult result = registry.registerVoter(person);
+		Assert.assertEquals(RegisterResult.VALID, result);
+	}
+	/*
+	* Prueba donde se crea una persona se intenta registrar
+	*/
+	@Test
+    public void validateRegistryResultDuplicatedAdult() {
+        Person person = new Person("Andres", 100220848, 52, Gender.MALE , false);
+        RegisterResult result = registry.registerVoter(person);
+        Assert.assertEquals(RegisterResult.DEAD, result);
+    }
+	/*
+	* Prueba donde se crea una persona duplicada y se intenta registrar
+	*/
+	@Test
+    public void validateRegistryResultDuplicatedYoung() {
+        Person person = new Person("Andres", 100220848, 22, Gender.MALE , false);
+        RegisterResult result = registry.registerVoter(person);
+        Assert.assertEquals(RegisterResult.DEAD, result);
+    }	
+	
 }
